6e771b42386f810f93b82bef183add2f8df1e1d9
Anastasiya.Honcharyk <honcharyk19@gmail.com>
2024-11-09 16:08:55 -0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header id="header">
+    <img src="/src/assets/investment-calculator-logo.png" alt="Logo showing a money bag"></img> 
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+  }
